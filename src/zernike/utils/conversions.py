eff88f1a1e8eaf353347259a61b60ef11c33c79e
Ali Taqi <alitaqi94.developer@gmail.com>
2024-10-21 10:29:17 +0100

    cartesian_to_polar() added

--- src/zernike/utils/conversions.py
+++ src/zernike/utils/conversions.py
@@ -1,6 +1,27 @@
 import numpy as np
 
 
+def cartesian_to_polar(
+        x: float, y: float
+) -> tuple[float, float]:
+    """
+    Converts Cartesian coordinates to polar coordinates.
+
+    Arguments
+    ---------
+    x: float
+        horizontal coordinate.
+    
+    y: float
+        vertical coordinate.
+
+    Returns
+    -------
+    Tuple bearing polar coordinates (r, theta - in radians).
+    """
+    return np.sqrt(x**2. + y**2.), np.arctan2(y, x)
+
+
 def polar_to_cartesian(
         r: float, theta: float
 ) -> tuple[float, float]:
