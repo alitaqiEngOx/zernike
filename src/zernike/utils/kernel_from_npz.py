e2c1b0905ed71f55c4384a1966a347e193f04d53
Ali Taqi <alitaqi94.developer@gmail.com>
2026-07-07 14:58:36 +0400

    `kernel_from_npz` added

---
+++ src/zernike/utils/kernel_from_npz.py
@@ -0,0 +1,11 @@
+""" Licensed under the same terms as described in the main 
+licensing script of this repository. """
+
+from pathlib import Path
+
+
+def extract(
+        path: Path, *, save_as: Path
+) -> None:
+    """
+    """
