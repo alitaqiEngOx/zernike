bfa832cf991b0a4b2b6a96ee2657e56f6a3132a8
Ali Taqi <alitaqi94.developer@gmail.com>
2026-07-07 14:54:01 +0400

    `extract_kernel_from_npz` app introduced

---
+++ src/zernike/apps/extract_kernel_from_npz.py
@@ -0,0 +1,37 @@
+""" Licensed under the same terms as described in the main 
+licensing script of this repository. """
+
+import argparse
+import sys
+
+
+def main() -> int:
+    """
+    """
+    args = parse_args()
+
+    return 0
+
+def parse_args() -> argparse.Namespace:
+    """
+    """
+    parser = argparse.ArgumentParser(
+        description="Extracts kernel from `.npz`",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        "path",
+        type=int,
+        help="`.npz` path"
+    )
+    parser.add_argument(
+        "--index",
+        type=int,
+        nargs='*',
+        help="position indices for the kernel to be extracted"
+    )
+
+
+if __name__ == "__main__":
+    sys.exit(main())
